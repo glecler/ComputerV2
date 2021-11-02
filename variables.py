4eab79cda1fa1c8b50b165aadfb84dd7e6c42e2f
Gregoire Lecler <gregoirelecler@gmail.com>
2021-11-02 10:49:07 +0100

    removed unkown class, added class methods

--- variables.py
+++ variables.py
@@ -1,202 +1,311 @@
 from __future__ import annotations
 from error import Error
 
-class Variable:
-	def __init__(self) -> None:
-		pass
-
-class Unknown(Variable):
-	def __init__(self, name: str) -> None:
-		self.name = name.strip()
-	
-	def __add__(self, other):
-		name = self.name + ' + ' + str(other)
-		return Unknown(name)
-
-	def __sub__(self, other):
-		name = self.name + ' - ' + str(other)
-		return Unknown(name)
-
-	def __rsub__(self, other):
-		name = str(other) + ' - ' + self.name
-		return Unknown(name)
-
-	def __mul__(self, other):
-		name = self.name + ' * ' + str(other)
-		return Unknown(name)
-	
-	def __rmul__(self, other):
-		name = self.name + ' * ' + str(other)
-		return Unknown(name)
-	
-	#def __div__(self, toher)
-	def __repr__(self):
-		return(f"{self.name}")
-
-	def __str__(self):
-		return(f"{self.name}")
-
-class Rationnals(Variable):
-	def __init__(self, name: str, input: float) -> None:
+class Rationnals :
+	def __init__(self, name : str, input : float) -> None :
 		self.real = input
 		self.name = name
 
-	def __add__(self, other: Rationnals) -> Rationnals | Imaginaries:
-		if type(other) is Error:
+	def __add__(self, other) :
+		if type(other) is Error or type(other) is Matrix :
 			return NotImplemented
-		if type(other) is Unknown:
-			return other + self
 		real = self.real + other.real
-		if type(other) is Imaginaries:
-			return Imaginaries("sum", real, other.img)
-		return Rationnals("sum", real)
+		if type(other) is Imaginaries :
+			return Imaginaries('sum', real, other.img)
+		return Rationnals('sum', real)
 
-	def __sub__(self, other: Rationnals) -> Rationnals | Imaginaries:
-		if type(other) is Error:
+	def __radd__(self, other) :
+		if type(other) is Error or type(other) is Matrix:
 			return NotImplemented
-		if type(other) is Unknown:
+		real = self.real + other.real
+		if type(other) is Imaginaries :
+			return Imaginaries('sum', real, other.img)
+		return Rationnals('sum', real)
+
+	def __sub__(self, other) :
+		if type(other) is Error :
 			return NotImplemented
 		real = self.real - other.real
-		if type(other) is Imaginaries:
-			return Imaginaries("sum", real, -other.img)
-		return Rationnals("sum", real)
+		if type(other) is Imaginaries :
+			return Imaginaries('sum', real, -other.img)
+		return Rationnals('sum', real)
 	
-	def __rsub__(self, other: Rationnals) -> Rationnals | Imaginaries:
-		if type(other) is Error:
+	def __rsub__(self, other) :
+		if type(other) is Error :
 			return NotImplemented
-		if type(other) is Unknown:
-			return other - self
 		real = other.real - self.real
-		if type(other) is Imaginaries:
-			return Imaginaries("sum", real, other.img)
-		return Rationnals("sum", real)
+		if type(other) is Imaginaries :
+			return Imaginaries('sum', real, other.img)
+		return Rationnals('sum', real)
 	
-	def __rmul__(self, other) -> Rationnals:
-		if type(other) is Error:
+	def __rmul__(self, other) :
+		if type(other) is Matrix :
 			return NotImplemented
+		if type(other) is Error :
+			return Error(other.err_code)
 		real = 0
 		img = 0
-		if type(other) is Unknown:
-			return other * self
-		if (type(other) is Rationnals):
+		if type(other) is Rationnals :
 			real = self.real * other.real
-		if type(other) is Imaginaries:
+		if type(other) is Imaginaries :
 			real = self.real * other.real
 			img = other.img * self.real
-			return Imaginaries("rmul", real, img)
-		return Rationnals("rmul", real)
+			return Imaginaries('rmul', real, img)
+		return Rationnals('rmul', real)
 	
-	def __mul__(self, other) -> Rationnals:
-		if type(other) is Error:
+	def __mul__(self, other) :
+		if type(other) is Matrix :
 			return NotImplemented
-		real = 0
-		img = 0
-		if type(other) is Unknown:
-			return other * self
-		if type(other) is Rationnals:
+		if type(other) is Error :
+			return Error(other.err_code)
+		real, img = 0, 0
+		if type(other) is Rationnals :
 			real = self.real * other.real
-		if type(other) is Imaginaries:
+		if type(other) is Imaginaries :
 			real = self.real * other.real
 			img = other.img * self.real
-			return Imaginaries("mul", real, img)
-		return Rationnals("mul", real)
+			return Imaginaries('mul', real, img)
+		return Rationnals('mul', real)
 
-	def __str__(self):
-		return(f"{self.real}")
+	def __truediv__(self, other) :
+		if type(other) is Error :
+			return NotImplemented
+		if type(other) is (float or int) and other != 0 :
+			return Rationnals('', self.real/other)
+		elif type(other) is (float or int) and other == 0 :
+			return Error(11) 
+		if type(other) is Rationnals and other.real != 0 :
+			return Rationnals('', self.real / other.real)
+		if type(other) is Rationnals and other.real == 0 :
+			return Error(11)
+		if type(other) is Imaginaries :
+			return NotImplemented
+		return self
+		
+	def __rtruediv__(self, other) :
+		if type(other) is Error :
+			return NotImplemented
+		if type(other) is (float or int) and other != 0 :
+			return Rationnals('',other/self.real)
+		elif type(other) is (float or int) and other == 0 :
+			return Error(11)
+		if type(other) is Rationnals and self.real == 0 :
+			return Error(11)
+		if type(other) is Rationnals and self.real != 0 :
+			return Rationnals('',other.real / self.real)
+		return self
 	
-	def __repr__(self):
-		return(f"{self.real}")
+	def __mod__(self, other) :
+		if type(other) is Error :
+			return NotImplemented
+		if type(other) is (float or int) and other != 0 :
+			return Rationnals('', self.real % other)
+		elif type(other) is (float or int) and other == 0 :
+			return Error(11)
+		if type(other) is Rationnals :
+			return Rationnals('', self.real % other.real)
+		if type(other) is Imaginaries :
+			return NotImplemented
+	
+	def __pow__(self, other) :
+		if type(other) is (Error or Imaginaries) :
+			return NotImplemented
+		if type(other) is (int or float) :
+			return Rationnals('', pow(self.real, other))
+		if type(other) is Rationnals :
+			return Rationnals('', pow(self.real, other.real))
+	
+	def __str__(self) :
+		return(f'{self.real}')
+	
+	def __repr__(self) :
+		return(f'{self.real}')
+	
+	def copy(self) :
+		return Rationnals('', self.real)
 
 
-class Imaginaries(Variable):
-	def __init__(self, name: str, realpart: float, imgpart: float) -> None:
+class Imaginaries :
+	def __init__(self, name : str, realpart : float, imgpart : float) -> None:
 		self.name = name
 		self.real = realpart
 		self.img = imgpart
 	
-	def __add__(self, other: Imaginaries | Rationnals) -> Imaginaries:
-		if type(other) is Error:
+	def __add__(self, other) :
+		if type(other) is Error :
 			return NotImplemented
-		name = "sum"
 		real = self.real + other.real
-		if type(other) is Imaginaries:
-			img = self.img + other.img
-		else:
-			img = self.img
-		return Imaginaries(name, real, img)
+		img = self.img + other.img \
+			if type(other) is Imaginaries else self.img
+		return Imaginaries('', real, img)
 
-	def __sub__(self, other):
-		if type(other) is Error:
+	def __sub__(self, other) :
+		if type(other) is Error :
 			return NotImplemented
-		name = "sub"		
 		real = self.real - other.real
-		if type(other) is Imaginaries:
-			img = self.img - other.img
-		else:
-			img = self.img
-		return Imaginaries(name, real, img)
+		img = self.img - other.img \
+			if type(other) is Imaginaries else self.img
+		return Imaginaries('', real, img)
 	
-	def __rsub__(self, other):
-		if type(other) is Error:
+	def __rsub__(self, other) :
+		if type(other) is Error :
 			return NotImplemented
-		name = "sub"
 		real = other.real - self.real
-		if type(other) is Imaginaries:
-			img = other.img - self.img
-		else:
-			img = -self.img
-		return Imaginaries(name, real, img)
+		img = other.img - self.img \
+			if type(other) is Imaginaries else -self.img
+		return Imaginaries('', real, img)
 
-	def __rmul__(self, other) -> Rationnals:
-		if type(other) is Error:
+	def __truediv__(self, other) :
+		if type(other) is Error :
+			return NotImplemented
+		if type(other) is (float or int) and other != 0 :
+			return Imaginaries('', self.real / other, self.img / other)
+		elif type(other) is (float or int) and other == 0 :
+			return Error(11)
+		if type(other) is Rationnals :
+			return Imaginaries('', self.real / other.real, self.img / other.real)
+		if type(other) is Imaginaries :
+			return Imaginaries('', (self.real * other.real + other.img * self.img) / (other.real**2 + other.img**2), \
+				(other.real * self.img - self.real * other.img) / (other.real**2 + other.img**2))
+		return self
+		
+	def __rtruediv__(self, other) :
+		if type(other) is Error :
+			return NotImplemented
+		if type(other) is (float or int) and other != 0 :
+			return Imaginaries('', other * self.real / (self.real**2 + self.img**2),\
+				-other * self.img / (self.real**2 + self.img**2))
+		elif type(other) is (float or int) and other == 0 :
+			return Error(11)
+		if type(other) is Rationnals :
+			return Imaginaries('', other.real * self.real / (self.real**2 + self.img**2), \
+				-other.real * self.img / (self.real**2 + self.img**2))
+		if type(other) is Imaginaries :
+			return Imaginaries('', (other.real * self.real + self.img * other.img) / (self.real**2 + self.img**2), \
+				(self.real * other.img - other.real * self.img) / (self.real**2 + self.img**2))
+		return self
+
+	def __rmul__(self, other) :
+		if type(other) is Error :
 			return NotImplemented
 		real = 0
 		img = 0
-		if type(other) is Rationnals:
+		if type(other) is Rationnals :
 			real = self.real * other.real
 			img = self.img * other.real
-		if type(other) is Imaginaries:
+		if type(other) is Imaginaries :
 			real = other.real * self.real - other.img * self.img 
 			img = other.img * self.real + other.real * self.img
-			return Imaginaries("rmul", real, img)
-		return Imaginaries("rmul", real, img)
+			return Imaginaries('rmul', real, img)
+		return Imaginaries('rmul', real, img)
 	
-	def __mul__(self, other) -> Rationnals:
-		if type(other) is Error:
+	def __mul__(self, other) :
+		if type(other) is Error :
 			return NotImplemented
 		real = 0
 		img = 0
-		if type(other) is Rationnals:
+		if type(other) is Rationnals :
 			real = self.real * other.real
 			img = self.img * other.real
-		if type(other) is Imaginaries:
+		if type(other) is Imaginaries :
 			real = self.real * other.real - self.img * other.img 
 			img = other.img * self.real + other.real * self.img
-			return Imaginaries("rmul", real, img)
-		return Imaginaries("rmul", real, img)
+			return Imaginaries('rmul', real, img)
+		return Imaginaries('rmul', real, img)
+	
+	def __pow__(self, other) :
+		return NotImplemented
 
-	def __repr__(self):
-		return(f"[{self.real} , {self.img}]")
+	def __mod__(self, other) :
+		return Error(10)
 
-	def __str__(self):
-		return(f"{self.real} + i * {self.img}")
+	def __rmod__(self, other) :
+		return Error(10)
+
+	def __repr__(self) :
+		return f'[{self.real} , {self.img}]'
+
+	def __str__(self) :
+		img_part = f'i * {self.img}' if self.img != 1 else f'i'
+		real_part = f'{self.real} + ' if self.real != 0 else ''
+		return real_part + img_part
+	
+	def copy(self) :
+		return Imaginaries('', self.real, self.img)
 
 
-class Matrix(Variable):
-	#check format matrix
+class Matrix :
+
 	def __init__(self, name, input) -> None:
 		self.mat = list()
 		self.name = name
-		rows = input.split(";")
-		for i in range(len(rows)):
-			rows[i] = rows[i].strip("]").strip("[").split(',')[0], rows[i].strip("]").strip("[").split(',')[1]
+		self.input = input
+		rows = input.split(';')
+		for i in range(len(rows)) :
+			rows[i] = rows[i].strip().strip(']').strip('[').split(',')
+			for j in range(len(rows[i])) :
+				rows[i][j] = float(rows[i][j])
 			self.mat.append(rows[i])
-		print(self.mat)
+
+	def __add__(self, other) :
+		return Error(13)
 	
-#img = Imaginaries("A", 2, 2)
-#reel = Rationnals("B", 4)
-#if issubclass(type(img), Variable):
-#	print(img)
-#print(reel+reel)
-#print(img+reel) # img
-#print(reel+img) # reel
+	def __radd__(self, other) :
+		return Error(13)
+
+	def __sub__(self, other) :
+		return Error(13)
+	
+	def __rsub__(self, other) :
+		return Error(13)
+	
+	def __pow__(self, other) :
+		return Error(13)
+	
+	def __rpow__(self, other) :
+		return Error(13)
+
+	def __mul__(self, other) :
+		mul_mat = []
+		if type(other) is Rationnals :
+			real = other.real
+		if type(other) is (float or int) :
+			real = other
+		buff = '['
+		for i in self.mat :
+			buff += '['
+			for j in i :
+				j *= real
+				buff += str(j) + ','
+			buff = buff[:-1]
+			buff += '];'
+		buff = buff[:-1]
+		buff += ']'
+		return Matrix('', buff) 
+
+	def __rmul__(self, other) :
+		mul_mat = []
+		if type(other) is Rationnals :
+			real = other.real
+		if type(other) is (float or int) :
+			real = other
+		buff = '['
+		for i in self.mat :
+			buff += '['
+			for j in i:
+				j *= real
+				buff += str(j) + ','
+			buff = buff[:-1]
+			buff += '];'
+		buff = buff[:-1]
+		buff += ']'
+		return(Matrix('', buff)) 
+
+	def __str__(self) :
+		buff = ''
+		for i in self.mat :
+			buff += str(i) + '\n'
+		return f'{buff}'
+	
+	def copy(self) :
+		return Matrix('', self.input)
