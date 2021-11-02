f569fdd62838355abc69612db3283787451f639f
Gregoire Lecler <gregoirelecler@gmail.com>
2021-11-02 10:47:17 +0100

    Added cos and sin functions

--- function.py
+++ function.py
@@ -1,15 +1,23 @@
 #!/usr/bin/python3
+import math as mt
 
-class Function():
+class Function :
 
-    def __init__(self, name, input, arglist):
+    def __init__(self, name, input) :
         self.name = name.split('(', 1)[0]
         self.arg = name.split('(')[1].split(')')[0]
         self.expression = input
-        self.arglist = arglist
-        print(f"{self.name}({self.arg}) = {self.expression}")
         
+    def image(self, x : str) -> str :
+        return self.expression.replace(self.arg, str(x))
+        
+    def __str__(self) :
+        return f'{self.expression}'
+
+class Cosinus(Function) :
     def image(self, x: str) -> str:
-        y = self.expression.replace('x', str(x))
-        # reduce (-- => +)
-        return(y)
+        return str(mt.cos(float(x)))
+
+class Sinus(Function) :
+    def image(self, x: str) -> str:
+        return str(mt.sin(float(x)))
